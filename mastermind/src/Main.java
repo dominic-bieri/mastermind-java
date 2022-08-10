61bc15f6de233cccf4f77838af2598302d75b7cf
Dominic Bieri <dominic.bieri@sunrise.ch>
2022-08-10 10:53:37 +0200

    finish Color-Match

--- mastermind/src/Main.java
+++ mastermind/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,7 +15,7 @@ public class Main {
             white   = 5
             black   = 6
          */
-
+        checkUserInputAgainstColorCode();
         ArrayList<int[]> tries = new ArrayList<int[]>();
 
         int[] colors = {1, 2, 3, 4};
@@ -34,7 +35,7 @@ public class Main {
 
     private static int[] promptUserColorCode() {
         //TODO: Prompt user to state custom color code
-        int[] userColorCode = {0, 0, 0, 0};
+        int[] userColorCode = new int[4];
         Scanner scanner = new Scanner(System.in);
         System.out.println("1 = Blue | 2 = Red | 3 = Green | 4 = Yellow | 5 = White | 6 = Black");
         for (int i = 0; i < 4; i++) {
@@ -57,29 +58,45 @@ public class Main {
     private static void checkUserInputAgainstColorCode() {
         //TODO: Check User Input against Color Code
 
+        if (checkIfPositionMatches() == 4){
+            System.out.println("YOU WON");
+        }
+        else{
+            System.out.println("Correct Colors: " + checkIfColorMatches() + "\t Correct Positions: " + checkIfPositionMatches());
+        }
+
     }
 
-    private static int checkIfColorMatches(/*int[] masterCode, int[] tryCode*/) {
-        int[] tryCode = {1, 2, 3 ,4};
-        int[] masterCode = {1, 2, 2, 6};
+    private static int checkIfColorMatches(/*int[] masterCode, int[] userAttemptCode*/) {
+        int[] userAttemptCode = {1, 2, 3 ,4};
+        int[] masterCode = {1, 1, 2, 6};
         //TODO: Check if a color matches
-        int matches = 0;
+        ArrayList<Integer> matches = new ArrayList<>();
+
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                if (tryCode[i] == masterCode[j]){
-                    if (){
-                        matches++;
+                if (userAttemptCode[j] == masterCode[i]){
+                    if (!matches.contains(userAttemptCode[j])){
+                        matches.add(userAttemptCode[j]);
                     }
                 }
             }
         }
-        System.out.println(matches);
-        return matches;
+        return matches.size();
     }
 
-    private static int checkIfPositionMatches() {
+    private static int checkIfPositionMatches(/*int[] masterCode, int[] userAttemptCode*/) {
         //TODO: Check if a position matches
-        return 0;
+        int[] masterCode = {1, 2 , 3 , 4};
+        int[] userAttemptCode = {1, 1, 4, 3};
+
+        int matches = 0;
+        for (int i = 0; i < 4; i++) {
+            if (userAttemptCode[i] == masterCode[i]){
+                matches++;
+            }
+        }
+        return matches;
     }
 
 }
